7832044c5fec1e3bc9a6ccd6ddd118db0d0dc83a
Administrator <Administrator@i7E-13>
2025-02-13 17:49:02 +0900

    s

--- sql/0213_2.sql
+++ sql/0213_2.sql
@@ -8,12 +8,12 @@ FI_PRICE INT not null
 
 drop table FOOD_INFO;
 
-insert into FOOD_INFO(FI_NUM,FI_NAME,FI_PRICE)
-VALUES(1,'김밥',3500),
-(2,'떡볶이',4000),
-(3,'순대',4500),
-(4,'라면',5000),
-(5,'해장라면',6000);
+insert into FOOD_INFO(FI_NAME,FI_PRICE)
+VALUES('김밥',3500),
+('떡볶이',4000),
+('순대',4500),
+('라면',5000),
+('해장라면',6000);
 
 select * from FOOD_INFO;
 
@@ -25,8 +25,12 @@ where FI_PRICE>=4000 and FI_NAME like '%라%';
 update FOOD_INFO
 set FI_PRICE = FI_PRICE + 500;
 delete from FOOD_INFO
-where FI_NUM>=6
+where FI_NUM >= 6
 select * from FOOD_INFO;
 
 update FOOD_INFO
 set FI_PRICE = FI_PRICE * 1.1;
+
+select * from FOOD_INFO 
+order by FI_NAME asc,
+ FI_PRICE ASC
